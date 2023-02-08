052890db7076dcd708625ee26951904d98613612
ZatokaV <zatokav@gmail.com>
2023-02-09 00:20:12 +0200

    Final commit

--- quotes/quotesapp/views.py
+++ quotes/quotesapp/views.py
@@ -1,3 +1,5 @@
+from collections import Counter
+
 from django.contrib.auth.decorators import login_required
 from django.core.paginator import Paginator
 from django.shortcuts import render, redirect, get_object_or_404
@@ -6,16 +8,23 @@ from .forms import TagForm, AuthorForm, QuoteForm
 from .models import Tag, Author, Quote
 
 
-# Create your views here.
-
-def change_name(name):
-    return name.replace(" ", "-")
-
-
 def main(request):
     quotes = Quote.objects.all()
-
-    return render(request, "quotesapp/index.html", context={"quotes": quotes})
+    # populars tags ----------------------->
+    all_tags = []
+    most_popular_tag = []
+    for el in quotes:
+        for tag in el.show_quote_tags():
+            all_tags.append(tag)
+    top_tags = Counter(all_tags).most_common(10)
+    for top in top_tags:
+        most_popular_tag.append(top[0])
+    # paginator --------------------------->
+    paginator = Paginator(quotes, 10)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+    return render(request, "quotesapp/index.html",
+                  context={"quotes": quotes, "most_popular_tag": most_popular_tag, 'page_obj': page_obj})
 
 
 @login_required
